859671fe26dc5e55f6591b139857c9f65d090d2e
RonaldKiprotich <protich12@gmail.com>
2020-12-10 01:09:18 +0300

    add urls for profile and profile update

--- groceapp/urls.py
+++ groceapp/urls.py
@@ -11,4 +11,6 @@ urlpatterns = [
     path('accounts/register/', views.registration, name='register'),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
+    path('profile/<username>/',views.profile, name='profile'),
+    path('profile/<username>/update/',views.update_profile, name='update_profile'), 
 ]
