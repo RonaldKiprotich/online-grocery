520a5318c0c99fe6b21ba13408b46e31bac452ce
RonaldKiprotich <protich12@gmail.com>
2020-12-09 20:08:11 +0300

    add database configuration

--- groceproject/settings.py
+++ groceproject/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'groceproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'grocery',
+        'USER': 'moringa',
+    'PASSWORD':'Raykip254',
     }
 }
 
