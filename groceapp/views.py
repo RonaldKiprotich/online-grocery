c84da3aae9eda12be84b65b945ba0ccc86c5f177
RonaldKiprotich <protich12@gmail.com>
2020-12-09 20:07:13 +0300

    add registration method

--- groceapp/views.py
+++ groceapp/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.decorators import login_required
+from .forms import *
+from .models import *
+from django.contrib import messages
 
 # Create your views here.
 def registration(request):
