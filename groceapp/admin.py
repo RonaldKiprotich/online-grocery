9a161f97529e2183fbfb85a242065865ec2b19ef
RonaldKiprotich <protich12@gmail.com>
2020-12-10 01:10:41 +0300

    register classes in admin site

--- groceapp/admin.py
+++ groceapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+
+
+admin.site.register(UserProfile)
+admin.site.register(Category)
+
+admin.site.register(User)
+admin.site.register(Employee)
+admin.site.register(Customer)
+
+
+
