9c5bdce3fcd214ea61fa1e863f7c934c73d4feb1
RonaldKiprotich <protich12@gmail.com>
2020-12-10 01:14:57 +0300

    add profile and edit profile methods

--- groceapp/views.py
+++ groceapp/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from .forms import *
 from .models import *
 from django.contrib import messages
+from cloudinary.models import CloudinaryField
 
 # Create your views here.
 def registration(request):
@@ -38,4 +39,28 @@ def home(request):
     }
 
     return render(request, 'index.html', params)
+
+def profile(request, username):
+    try:
+        user = User.objects.get(pk = username)
+        profile = UserProfile.objects.get(user = user)
+        units = Unit.get_user_units(profile.id)
+        units_count = units.count()
+    except Unit.DoesNotExist:
+        units = None
+    return render(request, 'profile.html',{'units': units, 'count': units_count})
+    
+def update_profile(request, username):
+    user = User.objects.get(username=username)
+    if request.method == 'POST':
+        edit_form = EditProfileForm(request.POST, request.FILES, instance=request.user.userprofile)
+        if edit_form.is_valid():
+            edit_form.save()
+            return redirect('profile', user.id)
+    else:
+        edit_form = EditProfileForm(instance=request.user.userprofile) 
+    context = {
+        'profile_form': edit_form,
+    }           
+    return render(request, 'edit_profile.html',context)
 
