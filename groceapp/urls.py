c5d4078b5c91e364c27ba737813619f60a99afb5
RonaldKiprotich <protich12@gmail.com>
2020-12-09 23:04:00 +0300

    add home urls

--- groceapp/urls.py
+++ groceapp/urls.py
@@ -7,6 +7,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 
 urlpatterns = [
+    path('',views.home,name='home'),
     path('accounts/register/', views.registration, name='register'),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
